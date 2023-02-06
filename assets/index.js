872d03bf2c3e1c46671b12e3cd678f854f1a898f
Iago-Santos-Sousa <iago.santos.sousa@mail.com>
2023-02-06 15:24:22 -0300

    Apresentação

--- assets/index.js
+++ assets/index.js
@@ -1,45 +1,55 @@
-const imgs = document.querySelectorAll('.skill')
-const data = document.querySelector('.data')
+const imgs = document.querySelectorAll(".skill");
+const data = document.querySelector(".data");
 
 // descrições
 
-const descricao = document.querySelector('.descricao')
+const descricao = document.querySelector(".descricao");
 
-const descricaoSkill = ['HTML', 'CSS', 'Javascript', 'Sass', 'Git', 'Github']
+const descricaoSkill = [
+  "HTML",
+  "CSS",
+  "Javascript",
+  "Sass",
+  "Git",
+  "Github",
+  "React",
+];
 
 const imagensMostrar = [
-  './imagens/html-svgrepo-com.svg',
-  './imagens/css-3-svgrepo-com.svg',
-  './imagens/javascript-logo-svgrepo-com.svg',
-  './imagens/sass-svgrepo-com-(1).svg',
-  './imagens/git-svgrepo-com-(1).svg',
-  './imagens/github-code-source-svgrepo-com.svg'
-]
+  "./imagens/html-svgrepo-com.svg",
+  "./imagens/css-3-svgrepo-com.svg",
+  "./imagens/javascript-logo-svgrepo-com.svg",
+  "./imagens/sass-svgrepo-com-(1).svg",
+  "./imagens/git-svgrepo-com-(1).svg",
+  "./imagens/github-code-source-svgrepo-com.svg",
+  "./imagens/react-svgrepo-com (1).svg",
+];
 
 const imagensDeixar = [
-  './imagens/html5-svgrepo-com.svg',
-  './imagens/css3-svgrepo-com.svg',
-  './imagens/javascript-svgrepo-com.svg',
-  './imagens/sass-svgrepo-com.svg',
-  './imagens/git-svgrepo-com.svg',
-  './imagens/github-svgrepo-com.svg'
-]
+  "./imagens/html5-svgrepo-com.svg",
+  "./imagens/css3-svgrepo-com.svg",
+  "./imagens/javascript-svgrepo-com.svg",
+  "./imagens/sass-svgrepo-com.svg",
+  "./imagens/git-svgrepo-com.svg",
+  "./imagens/github-svgrepo-com.svg",
+  "./imagens/react-svgrepo-com.svg",
+];
 
 imgs.forEach((e, i) => {
-  e.addEventListener('mouseover', (ev) => {
-    descricao.innerHTML = `<p>${descricaoSkill[i]}</p>`
+  e.addEventListener("mouseover", (ev) => {
+    descricao.innerHTML = `${descricaoSkill[i]}`;
 
-    ev.target.src = imagensMostrar[i]
-    ev.target.style.cursor = 'pointer'
-  })
+    ev.target.src = imagensMostrar[i];
+    ev.target.style.cursor = "pointer";
+  });
 
-  e.addEventListener('mouseout', (ev) => {
-    ev.target.src = imagensDeixar[i]
+  e.addEventListener("mouseout", (ev) => {
+    ev.target.src = imagensDeixar[i];
 
-    descricao.innerHTML = `${'Passe o mouse'}`
-  })
-})
+    descricao.innerHTML = `${"Passe o mouse"}`;
+  });
+});
 
-const ano = new Date().getFullYear()
+const ano = new Date().getFullYear();
 
-data.innerHTML = `Copyright &copy; ${ano} Iago`
+data.innerHTML = `Copyright &copy; ${ano} Iago`;
